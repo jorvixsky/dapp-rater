6dcd94fbd820a477a09e9f871b102cd1795228c6
jorvixsky <jorvixsky@proton.me>
2024-02-07 19:38:28 +0100

    Configured Rainbowkit

--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -1,4 +1,11 @@
 /** @type {import('next').NextConfig} */
-module.exports = {
-  transpilePackages: ["@repo/ui"],
+const nextConfig = {
+  reactStrictMode: true,
+  webpack: config => {
+    config.resolve.fallback = { fs: false, net: false, tls: false };
+    config.externals.push('pino-pretty', 'lokijs', 'encoding');
+    return config;
+  },
 };
+
+module.exports = nextConfig;
